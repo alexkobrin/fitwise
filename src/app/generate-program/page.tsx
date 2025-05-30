2f0765ea784857870d39921e515f23d291d9ca41
Aliaksei <a.hramatsenka@interactivesystems.pl>
2025-05-30 15:35:02 +0200

    wip

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,12 @@
+ 
+const GenerateProgramPage = () => {
+  return (
+    <main  className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1>Generate Program</h1>
+      <p>Welcome to the program generator page. Start building your custom program here.</p>
+     
+    </main>
+  );
+};
+
+export default GenerateProgramPage;
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+  
+const ProfilePage = () => {
+  return (
+    <main>
+      <h1>Profile Page</h1>
+      <p>Welcome to your profile!</p>
+    </main>
+  );
+};
+
+export default ProfilePage;
