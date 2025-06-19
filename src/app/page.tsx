e4017f2be8b76345d0c72c870cc1d513c0dd4234
alexkobrin <storyday2012@gmail.com>
2025-06-19 21:56:00 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { SignedIn } from "@clerk/nextjs"
 const HomePage = () => {
   return (
     <div className='text-4xl flex  flex-col gap-10 font-bold text-center mt-20  '> 
-    
     <div className='text-2xl mt-10'>Welcome to Fitwise</div>
     <div className='text-lg mt-5'>Empower your fitness journey with digital precision.</div>
      
